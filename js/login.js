f2345b1e504e910a84f09b60b3e289a36d5fbd60
chang sik kwon <kcs1987@gamil.com>
2021-01-24 17:37:22 +0900

    img slide

--- js/login.js
+++ js/login.js
@@ -1,11 +1,39 @@
 const inputId = document.querySelector('.input-id')
 const inputPassword = document.querySelector('.input-password')
 const loginBtn = document.querySelector('.login-btn')
+const SHOWING_CLASS = "showing"
+const firstSlide = document.querySelector(".img:first-child")
+
+function slide(){
+
+  const currentSlide = document.querySelector(`.${SHOWING_CLASS}`)
+
+  if (currentSlide) {
+    currentSlide.classList.remove(SHOWING_CLASS)
+    const nextSlide = currentSlide.nextElementSibling
+
+    if (nextSlide) {
+      nextSlide.classList.add(SHOWING_CLASS)
+    } else {
+      firstSlide.classList.add(SHOWING_CLASS)
+    }
+
+  } else {
+    firstSlide.classList.add(SHOWING_CLASS)
+  }
+}
+
+slide();
+setInterval(slide, 5000);
+
 
 const updateValue = (e) => {
     if (inputId.value){
         if (inputPassword.value){
-            alert('로그인 성공!')
+            if (inputPassword.value.length <= 8 && !isNaN(inputPassword.value)){
+                alert('로그인 성공!')
+                location.replace('main.html')
+            }
         }
     }
 }
