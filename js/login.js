e9542b7f6f12f98392a02e424b917cc94c6bd904
chang sik kwon <kcs1987@gamil.com>
2021-01-24 17:47:32 +0900

    img slide

--- js/login.js
+++ js/login.js
@@ -5,19 +5,16 @@ const SHOWING_CLASS = "showing"
 const firstSlide = document.querySelector(".img:first-child")
 
 function slide(){
-
   const currentSlide = document.querySelector(`.${SHOWING_CLASS}`)
 
   if (currentSlide) {
     currentSlide.classList.remove(SHOWING_CLASS)
     const nextSlide = currentSlide.nextElementSibling
-
     if (nextSlide) {
       nextSlide.classList.add(SHOWING_CLASS)
     } else {
       firstSlide.classList.add(SHOWING_CLASS)
     }
-
   } else {
     firstSlide.classList.add(SHOWING_CLASS)
   }
