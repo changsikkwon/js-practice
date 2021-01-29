9cbfd23c4d98a339d59201a5bad0c7df44d32fd2
chang sik kwon <kcs1987@gamil.com>
2021-01-29 22:55:10 +0900

    react

---
+++ instagram/src/Pages/Login/login.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const login = () => {
+    const [account, setAccount] = useState("")
+    const [password, setPassword] = useState("")
+}
